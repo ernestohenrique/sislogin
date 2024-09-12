c3b171695fa1aa193c7f753f0dd94daa9b69927c
Ernesto Henrique <ernestohnm@gmail.com>
2024-09-12 07:41:17 -0300

    Primeira  versão atualizada

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyByYWL4lsn3YZGsRcPu7ih-0J60rNAZixE",
+    authDomain: "sistemateste-7a5ff.firebaseapp.com",
+    projectId: "sistemateste-7a5ff",
+    storageBucket: "sistemateste-7a5ff.appspot.com",
+    messagingSenderId: "673509130676",
+    appId: "1:673509130676:web:edda5329e72d82660b32e1",
+  },
+};
