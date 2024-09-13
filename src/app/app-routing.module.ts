7c379b9d3c9d841bdb59fa46133c1027bda9925b
Ernesto Henrique <ernestohnm@gmail.com>
2024-09-13 14:06:46 -0300

    Atualização das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from "./guards/auth.guard";
 import { MainComponent } from "./main/main.component";
 
 const routes: Routes = [
+  { path: "", redirectTo: "/main", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "main", component: MainComponent },
   {
@@ -16,22 +17,6 @@ const routes: Routes = [
 
   // Adiciona uma rota coringa para capturar todas as rotas não existentes
   { path: "**", redirectTo: "/main" },
-  /*{
-    path: "login",
-    loadComponent: () =>
-      import("./login/login.component").then((m) => m.LoginComponent),
-  },
-  { path: "main", component: MainComponent },
-  { path: "cadastrar", component: CadastrarComponent },
-  {
-    path: "logada",
-    loadChildren: () =>
-      import("./logada/logada-routing.module").then(
-        (m) => m.RestritoRoutingModule
-      ),
-  },
-  // Adiciona uma rota coringa para capturar todas as rotas não existentes
-  { path: "**", redirectTo: "/main" },*/
 ];
 
 @NgModule({
